0a4c876efdcd37587a537b4676e78b51002ce520
jwydth <s4079860@rmit.edu.vn>
2024-08-03 11:08:59 +0700

    update

---
+++ src/Pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Admin.css";
+
+const Admin = () => {
+  return (
+    <div className="admin">
+      <div></div>
+    </div>
+  );
+};
+
+export default Admin;
